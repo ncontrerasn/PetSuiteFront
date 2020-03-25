9741435af431883b66ca5eba69b3391f64e85534
Diego A. Velasquez Vargas <31147126+arvelasquezva@users.noreply.github.com>
2020-03-25 15:01:37 -0500

    Commit Inicial Vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Navbar></Navbar>
+  </div>
+</template>
+
+<script>
+import  Navbar  from "./components/navbar/index.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  },
+  data(){
+    return{
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
