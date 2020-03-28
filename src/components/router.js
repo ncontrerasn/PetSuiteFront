d723324e6a49d913a3accdf7dee9d7ef132c4c33
Diego A. Velasquez Vargas <31147126+arvelasquezva@users.noreply.github.com>
2020-03-27 19:32:13 -0500

    File Creation Vue Router

---
+++ src/components/router.js
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+
+Vue.use(Router);
+
+export default new Router({
+    linkExactActiveClass: 'active',
+    routes: [{
+            path: "/login",
+            name: "login",
+            component: "loginComponent"
+        },
+        {
+
+        },
+        {
+
+        },
+        {
+
+        }
+    ],
+});
