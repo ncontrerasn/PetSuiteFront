d434048ce3fc261665e2e58deb38334118acf283
josemiguelcarvajal07 <62575167+josemiguelcarvajal07@users.noreply.github.com>
2020-03-30 18:05:49 -0500

    experimentando con carousel

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -7,7 +7,9 @@ export default {
         return {
             pictures: ["https://images.pexels.com/photos/551628/pexels-photo-551628.jpeg",
                 "https://images.pexels.com/photos/825947/pexels-photo-825947.jpeg",
-                "https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg"
+                "https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg",
+                "https://d17fnq9dkz9hgj.cloudfront.net/breed-uploads/2018/09/dog-landing-hero-lg.jpg?bust=1536935129&width=1080",
+                "https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2019/02/dog-451643.jpg?h=bf654dbc&itok=MQGvBmuo"
             ]
         }
     },
